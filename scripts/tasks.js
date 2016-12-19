fd01a251c02021a3ad06a121d4dcf1a025d24f93
Otis Chou <altispec@gmail.com>
2016-12-19 22:28:54 +0800

    chore: add tachyons.css

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -8,4 +8,6 @@ exports.copyAssets = type => {
   cp(`chrome/manifest.${env}.json`, `${type}/manifest.json`)
   cp('-R', 'chrome/assets/*', type)
   cp('-R', 'chrome/views/*', type)
+  mkdir(`${type}/styles`)
+  cp('node_modules/tachyons/css/tachyons.css', `${type}/styles/tachyons.css`)
 }
